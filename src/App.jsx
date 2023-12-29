b40b83dee380023c687c7c8f4d90994594cc29fd
Angel Pureco <angelpureco@Angels-MacBook-Pro.local>
2023-12-29 10:52:09 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center'>hello</div>
+    </>
+  )
+}
+
+export default App
